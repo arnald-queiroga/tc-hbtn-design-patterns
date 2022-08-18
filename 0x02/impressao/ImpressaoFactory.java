1a1ad9d5fd9e888339859932a10b4a1703d0aa1c
Arnald da Costa Queiroga <CIANDT\arnald@lnb036081spo.ciandt.global>
2022-08-18 19:37:47 -0300

    Task 1

---
+++ 0x02/impressao/ImpressaoFactory.java
@@ -0,0 +1,5 @@
+public class ImpressaoFactory {
+    public static Impressao createImpressao( TamanhoImpressao tamanho, int totalPaginas, int quantidadesColoridas, boolean ehFrenteVerso){
+        return  new Impressao(tamanho, totalPaginas, quantidadesColoridas, ehFrenteVerso);
+    }
+}
