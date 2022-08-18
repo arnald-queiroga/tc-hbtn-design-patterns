e2783837dd3e6cb8995a9a6e67356394697d20bb
Arnald da Costa Queiroga <CIANDT\arnald@lnb036081spo.ciandt.global>
2022-08-18 19:25:22 -0300

    0. Serviço Lanchonete Pedido Builder

---
+++ 0x02/lanchonete/ItemPedido.java
@@ -0,0 +1,28 @@
+public class ItemPedido {
+    private TipoItemPedido tipo;
+    private String nome;
+
+    public ItemPedido(TipoItemPedido tipo, String nome) {
+        this.tipo = tipo;
+        this.nome = nome;
+    }
+
+    public ItemPedido() {
+    }
+
+    public TipoItemPedido getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoItemPedido tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
